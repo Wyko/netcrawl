d97344377e55879b6131c356599418b46134f6c3
Wyko <vegaswyko@gmail.com>
2017-03-29 01:33:12 +0200

    Troubleshooting path

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.join(os.path.abspath('..'), 'netcrawl'))
-
+print(sys.path)
 
 # -- General configuration ------------------------------------------------
 
